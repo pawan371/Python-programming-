b2a4d0d0571dcfc4908399e5a9f441f2b3f83bd5
Pawan Tiwari <pawantiwari3943@gmail.com>
2026-02-25 11:56:18 +0530

    input

---
+++ multipleinput.py
@@ -0,0 +1,8 @@
+x, y = input("Enter two numbers separated by space: ").split()#taking two inputs and splitting them into x and y ,split() method. 
+print("print first number:", x)
+print("print second number:", y)
+
+x, y, z =input("Enter three numbers :")split()#taking three inputs and splitting them into x,y and z ,split() method.
+print("print first number:", x)
+print("print second number:", y)    
+print("print third number:", z)
